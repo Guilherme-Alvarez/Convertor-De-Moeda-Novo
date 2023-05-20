d9442e47860619f3b3000d9de30ceae344465f75
Guilherme-Alvarez <guilhermealvarez46@gmail.com>
2023-05-19 21:42:05 -0300

    primeiro Commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const button = document.querySelector('button')
+const currencyAll = document.querySelector('#select-moeda')
+
+const dolar = 5
+const bitcoin = 134 
+const euro = 5.4
+const libra = 6.2
+
+const buttonClick = () =>{
+    const value = document.querySelector('#input-value').value
+    const realValue = document.querySelector('#real-value')
+    const valueFinal = document.querySelector('#value-final')
+
+    realValue.innerHTML = new Intl.NumberFormat('pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(value);
+
+    if (currencyAll.value === 'US$ Dólar Americano') {
+        valueFinal.innerHTML = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+        }).format(value / dolar)
+    }
+
+    if (currencyAll.value === '€ Euro') {
+        valueFinal.innerHTML = new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'EUR',
+        }).format(value / euro)
+    }
+
+    if (currencyAll.value === 'BTC Biticoin') {
+        valueFinal.innerHTML = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'BTC',
+        }).format(value / bitcoin)
+    }
+
+    if (currencyAll.value === '£ Libra') {
+        valueFinal.innerHTML = new Intl.NumberFormat('en-GB', {
+            style: 'currency',
+            currency: 'GBP',
+        }).format(value / libra)
+    }
+}
+
+
+
+const change = () =>{
+    const nameMoeda = document.querySelector('#name-moeda')
+    const flag = document.querySelector('#flag')
+
+    if(currencyAll.value === 'US$ Dólar Americano'){
+        nameMoeda.innerHTML = 'US$ Dólar Americano'
+        flag.src = './assets/estados-unidos (1) 1.png'
+    }
+
+    if(currencyAll.value === 'BTC Biticoin'){
+        nameMoeda.innerHTML = 'BTC Biticoin'
+        flag.src = './assets/bitcoin 1.png'
+    }
+
+    if(currencyAll.value === '£ Libra'){
+        nameMoeda.innerHTML = '£ Libra'
+        flag.src = './assets/libra 1.png'
+    }
+
+    if(currencyAll.value === '€ Euro'){
+        nameMoeda.innerHTML = '€ Euro'
+        flag.src = './assets/euro.png'
+    }
+
+    buttonClick()
+}
+
+currencyAll.addEventListener('change', change)
+button.addEventListener('click', buttonClick)
